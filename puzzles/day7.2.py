13c927f88ac7b6a603b78223a8e616a0e3d01318
Mara Schulze <Mara.Schulze@posteo.de>
2025-08-04 20:52:21 +0200

    Added day 7

---
+++ puzzles/day7.2.py
@@ -0,0 +1,47 @@
+from get_input import get_input, get_test_input
+
+
+def apply(wire, d):
+    if wire.isdigit():
+        return int(wire)
+    if wire.isalpha() and type(d[wire]) != list:
+        return apply(d[wire], d)
+
+    operation = d[wire]
+
+    op = operation[-2]
+    if op == "AND":
+        result = apply(operation[0], d) & apply(operation[-1], d) % 2**16
+    elif op == "OR":
+        result = apply(operation[0], d) | apply(operation[-1], d) % 2**16
+    elif op == "LSHIFT":
+        result = apply(operation[0], d) << apply(operation[-1], d) % 2**16
+    elif op == "RSHIFT":
+        result = apply(operation[0], d) >> apply(operation[-1], d) % 2**16
+    elif op == "NOT":
+        result = ~apply(operation[-1], d) % 2**16
+    else:
+        print("Error")
+
+    d[wire] = str(result)
+    return result
+
+# get input
+inp = get_input(__file__)
+d = {}
+for line in inp:
+    operation, wire = line.split(" -> ")
+    left_side = operation.split()
+    if len(left_side) == 1:
+        d[wire] = left_side[0]
+    else:
+        d[wire] = left_side
+
+# override wire b
+d["b"] = "956"
+
+# apply wires
+result = apply("a", d)
+
+# print result
+print(result)

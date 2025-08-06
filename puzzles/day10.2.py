e332e90ffb164faf2d26719e125212cffa6bce0a
Mara Schulze <Mara.Schulze@posteo.de>
2025-08-06 05:27:02 +0200

    Added day 10

---
+++ puzzles/day10.2.py
@@ -0,0 +1,28 @@
+from get_input import get_input, get_test_input
+
+
+def produce_sequence(sequence):
+    current_number = sequence[0]
+    length = 0
+    next_sequence = ""
+    for number in sequence:
+        if number == current_number:
+            length += 1
+        else:
+            next_sequence += str(length) + current_number
+            current_number = number
+            length = 1
+    next_sequence += str(length) + current_number
+    return next_sequence
+
+# get input
+inp = get_input(__file__)
+sequence = inp[0]
+
+# produce sequence
+for _ in range(50):
+    sequence = produce_sequence(sequence)
+result = len(sequence)
+
+# print result
+print(result)
